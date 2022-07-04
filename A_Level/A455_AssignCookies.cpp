eb41a39da90275c42cc0da9f7f722866f1b94904
minzziPark <minzzi0068@gmail.com>
2022-07-04 20:22:06 +0900

    Create A455_AssignCookies.cpp

---
+++ A_Level/A455_AssignCookies.cpp
@@ -0,0 +1,34 @@
+/*
+source : https://leetcode.com/problems/assign-cookies/
+
+Assume you are an awesome parent and want to give your children some cookies. 
+But, you should give each child at most one cookie.
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; 
+and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, 
+and the child i will be content. 
+Your goal is to maximize the number of your content children and output the maximum number.
+*/
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int output = 0;
+		vector<int>::iterator i;
+		vector<int> G = g;
+		vector<int> S = s;
+		if(g.size() > s.size()) output = s.size();
+		else output = g.size();
+
+		while(!S.empty()){
+			for(i=G.begin(); i!=G.end(); i++){
+				if(S[0] >= G.at(*i)){
+					S.erase(S.begin());
+					G.erase(i);
+					break;
+				}
+			}
+			if(!G.empty() && i == G.end()) output--;
+		}
+
+		return output;
+    }
+};
