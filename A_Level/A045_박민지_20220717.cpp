344e295d0e3733ac23d9fae4ed5da8b939582a7b
minzziPark <minzzi0068@gmail.com>
2022-07-17 16:55:34 +0900

    45 update

--- A_Level/A045_박민지_20220717.cpp
+++ A_Level/A045_박민지_20220717.cpp
@@ -13,6 +13,8 @@ Source: https://www.acmicpc.net/problem/1157
 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 */
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,9 +22,28 @@ int main(){
 	string str;
 
 	cin >> str;
-	str = toupper(str);
+	for(int i=0; i<str.size(); i++){
+		str[i] = toupper(str[i]);
+	}
+	int alpa[26] = {};
+	int max = 0;
+	char max_alpa = ' ';
+	bool same = true;
 
-	
+	for(int i=0; i<str.size(); i++){
+		alpa[str[i]-65]++;
+	}
+	for(int i=0; i<26; i++){
+		if(max < alpa[i]){
+			max = alpa[i];
+			max_alpa = i+65;
+			same = false;
+		}else if(max == alpa[i]){
+			same = true;
+		}
+	}
+	if(same) max_alpa = '?';
+	cout << max_alpa << endl;
 
 	return 0;
 }
